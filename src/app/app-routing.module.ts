576bb354a7d65979fbf3a32d00146ff5cbe619e4
Divya <ramdivyamadhu@gmail.com>
2021-09-26 22:22:02 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,66 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ExamPageComponent } from './exam-page/exam-page.component';
+import { ReportComponent } from './report/report.component';
+import { AdminProfileComponent } from './admin-profile/admin-profile.component';
+import { RemoveQuestionsComponent } from './remove-questions/remove-questions.component';
+import { AddQuestionsComponent } from './add-questions/add-questions.component';
+import { ViewReportsComponent } from './view-reports/view-reports.component';
+const routes: Routes = [
+  {
+    path:"home",
+    component:HomeComponent
+  },
+  {
+    path:"about us",
+    component:AboutUsComponent
+  },
+  {
+    path:"login",
+    component:LoginComponent
+  },
+  {
+    path:"register",
+    component:RegisterComponent
+  },
+  {
+    path:"reset password",
+    component:ResetPasswordComponent
+  },
+  {
+    path:"exam page",
+    component:ExamPageComponent
+  },
+  {
+    path:"report",
+    component:ReportComponent
+  },
+  {
+    path:"admin profile",
+    component:AdminProfileComponent
+  },
+  {
+    path:"add questions",
+    component:AddQuestionsComponent
+  },
+  {
+    path:"remove questions",
+    component:RemoveQuestionsComponent
+  },
+  {
+    path:"view reports",
+    component:ViewReportsComponent
+  }
 
-const routes: Routes = [];
+
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ExamPageComponent } from './exam-page/exam-page.component';
+import { ReportComponent } from './report/report.component';
+import { AdminProfileComponent } from './admin-profile/admin-profile.component';
+import { AddQuestionsComponent } from './add-questions/add-questions.component';
+import { RemoveQuestionsComponent } from './remove-questions/remove-questions.component';
+import { ViewReportsComponent } from './view-reports/view-reports.component';
+import { InstructionsComponent } from './instructions/instructions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    AboutUsComponent,
+    ResetPasswordComponent,
+    ExamPageComponent,
+    ReportComponent,
+    AdminProfileComponent,
+    AddQuestionsComponent,
+    RemoveQuestionsComponent,
+    ViewReportsComponent,
+    InstructionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
